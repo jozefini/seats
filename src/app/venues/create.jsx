9b1052fd1ba338e13a3cfcd363cb46038ea82df7
jozefini <jozibashaj@gmail.com>
2023-10-04 17:34:20 +0200

    Update

--- src/app/venues/create.jsx
+++ src/app/venues/create.jsx
@@ -5,9 +5,9 @@ import { Topbar } from './topbar'
 import dictionary from './i18n/create.json'
 
 const css = {
-	wrapper: 'max-w-7xl w-full mx-auto px-4',
+	wrapper: 'max-w-[90rem] w-full mx-auto px-4',
 	box: 'rounded-[0.5rem] border bg-white shadow',
-	layout: 'grid grid-cols-[3fr_1fr] gap-x-5 border-t min-h-[40rem]',
+	layout: 'grid grid-cols-[3fr_1fr] gap-x-5 border-t min-h-[50rem]',
 }
 
 export function CreateVenue({ onSubmit, data, isLoading }) {
